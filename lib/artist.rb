6af1907c19cae6175e4230f98c23c9af336e973d
Anthony DiPietrantonio <antdp425@aol.com>
2020-04-03 19:35:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,13 +12,13 @@ attr_accessor :name, :songs
 
   def add_song(song)
     @songs << song
-    song.artist = self.name
+    song.artist = self
   end
 
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
-    song.artist = self.name
+    song.artist = self
   end
 
   def self.song_count
