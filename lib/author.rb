9bda7c9522c53d0787294eeddce0ed8547dd509c
Anthony DiPietrantonio <antdp425@aol.com>
2020-04-03 19:39:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,8 +11,15 @@ def posts
 end
 
 def add_post(post)
-  @posts << post
   post.author = self
+  @posts << post
+end
+
+def add_post_by_title(post_title)
+  post = Post.new(post_title)
+  post.author = self
+  @posts << post
+
 end
 
 end
