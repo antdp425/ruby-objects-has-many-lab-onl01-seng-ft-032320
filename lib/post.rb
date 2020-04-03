8c39e4e7a156a1fcfc3e4ed569eb33f252bcfc37
Anthony DiPietrantonio <antdp425@aol.com>
2020-04-03 19:42:58 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,25 +2,25 @@ class Post
 attr_accessor :author, :title
 @@all
 
-def initialize(title)
-  @title = title
-  @@all << self
-end
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
-def title
-  @title
-end
+  def title
+    @title
+  end
 
-def author
-  @author
-end
+  def author
+    @author
+  end
 
-def author_name
-  self.author.name
-end
+  def author_name
+    self.author.name
+  end
 
 end
